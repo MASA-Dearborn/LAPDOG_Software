aaff03e51c24b4b6d56f4cedb801968af697904b
Lucasring <zanerlo50@gmail.com>
2021-12-22 19:55:39 +0000

    message types file updated

--- include/messageTypes.h
+++ include/messageTypes.h
@@ -3,9 +3,31 @@
 namespace msg
 {
 
-enum MessageType
-{
-    TEST_MESSAGE = 0,
-};
+    /* Enum Namespace */
+    namespace ids
+    {
+        enum MessageType
+        {
+            NONE = 0,
+            TEST_MESSAGE,
+        };
+    }
+
+    /* Message Declarations */
+    namespace types
+    {
+
+        struct TEST_MESSAGE
+        {
+            int test;
+        };
+
+    }
+
+    /* Overall Message Structure */
+    struct MessageCollection
+    {
+        types::TEST_MESSAGE TEST_MESSAGE;
+    };
 
 }
