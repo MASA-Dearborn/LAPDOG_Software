488336dbef77c63d56c4d6c7eb4cd37f564bb87b
Lucasring <zanerlo50@gmail.com>
2021-12-23 21:53:16 +0000

    Added method documentation to broker.cpp

--- src/broker/broker.cpp
+++ src/broker/broker.cpp
@@ -6,6 +6,10 @@ using namespace pubsub;
 
 Broker pubsub::DataBroker;
 
+/**
+ * @brief Destroy the Broker:: Broker object and clear all existing subscribers
+ * 
+ */
 Broker::~Broker()
 {
     for(int i = 0; i < (int)msg::ids::META_NUM_MESSAGES; i++)
@@ -14,16 +18,32 @@ Broker::~Broker()
     }
 }
 
+
+/**
+ * @brief   Registers a subscriber to the broker
+ * 
+ * @param   subscriber  Subscriber to register
+ */
 void Broker::registerSubscriber(GenericSubscriber* subscriber)
 {
     subscribers[subscriber->getType()].push_back(std::unique_ptr<GenericSubscriber>(subscriber));
 }
 
+/**
+ * @brief   Registers a publisher to the broker
+ * 
+ * @param   publisher   Publisher to register
+ */
 void Broker::registerPublisher(GenericPublisher* publisher)
 {
     publishers[publisher->getType()] = std::unique_ptr<GenericPublisher>(publisher);
 }
 
+/**
+ * @brief   Unregister a subscriber from the broker
+ * 
+ * @param   subscriber  Subscriber to remove
+ */
 void Broker::unregisterSubscriber(GenericSubscriber* subscriber)
 {
 
@@ -41,6 +61,11 @@ void Broker::unregisterSubscriber(GenericSubscriber* subscriber)
 
 }
 
+/**
+ * @brief   Unregister a publisher from the broker
+ * 
+ * @param   publisher   Publisher to remove
+ */
 void Broker::unregisterPublisher(GenericPublisher* publisher)
 {
     // TODO: Implement
