a5711e0af619802d120dbdf33d22372fbe82ca7f
Lucasring <zanerlo50@gmail.com>
2022-02-08 01:12:07 +0000

    I2C_Interface conversion

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 #include "IO/MessageHandler.h"
 #include "broker/broker.h"
 #include "IO/SPI.h"
+#include "IO/I2C.h"
 
 #include <unistd.h>
 #include <cstdio>
@@ -10,7 +11,8 @@ using namespace IO;
 int main()
 {
 
-    SPI_Interface temp;
+    SPI_Interface temp_spi;
+    I2C_Interface temp_i2c;
 
     while(true)
     {
