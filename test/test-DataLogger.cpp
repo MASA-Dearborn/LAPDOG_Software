64b0e1ab2d606be8b07d2c315993ddb739669a83
Lucasring <zanerlo50@gmail.com>
2022-02-17 22:43:47 +0000

    Added DataLogger single write testing

--- test/test-DataLogger.cpp
+++ test/test-DataLogger.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "datalogger/dataLogger.h"
+#include "broker/broker.h"
 
 void delete_file(const char* path)
 {
@@ -53,7 +56,33 @@ public:
 
 };
 
-TEST_F(DataLoggerFixture, temp)
+TEST_F(DataLoggerFixture, TestDataLoggerWrite)
 {
-    printf("Hello\n");
+    char file_path[1024];
+    char data_read[64] = {0};
+    char data_expected[64] = {0};
+    int retval = 0;
+
+    strcpy(file_path, dl->getLogFolderNamePtr());
+    strcat(file_path, "/TEST_MESSAGE/TEST_MESSAGE_0001.log");
+
+    // Send a message for logging
+    pubsub::Publisher<msg::real::TEST_MESSAGE>* pub = createNewPublisher(TEST_MESSAGE);
+    msg::real::TEST_MESSAGE send_message;
+    send_message.test = 5;
+    send_message.VAR2 = 0;
+    pub->publish(&send_message);
+
+    // Wait so datalogger timer can trigger (50ms)
+    usleep(100000);
+    dl->getLoggerPtr(msg::id::TEST_MESSAGE)->log->closeFile();
+    msg::conv::stringifyRealMessage(data_expected, &send_message);
+
+    // Open the file written to
+    int file_descriptor = open(file_path, O_RDWR);
+    EXPECT_TRUE(file_descriptor > 0);
+ 
+    retval = read(file_descriptor, data_read, 64);
+    EXPECT_STREQ(data_read, data_expected) << data_read << " != " << data_expected << "\n";
+
 }
