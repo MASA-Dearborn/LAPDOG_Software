50d3527e248aca92c4d3c3fb78d8f669a3e8af70
Lucasring <zanerlo50@gmail.com>
2022-03-04 00:45:32 +0000

    added altimeter and humidity sensor messages

--- include/messageTypes.h
+++ include/messageTypes.h
@@ -12,6 +12,9 @@ namespace msg::id {
     enum MessageType {
         TEST_MESSAGE = 0,
         TEST_MESSAGE_2 = 1,
+        ALTIMETER_COEFFS = 2,
+        ALTIMETER_DATA = 3,
+        HUMIDITY_DATA = 4,
         META_NUM_MESSAGES,
         UNDEFINED_MESSAGE,
     };
@@ -41,6 +44,28 @@ namespace msg::real {
         float VAR2 = 0.23;
     };
     
+    struct ALTIMETER_COEFFS : GENERIC_MESSAGE {
+        ALTIMETER_COEFFS() { id = msg::id::ALTIMETER_COEFFS; size = sizeof(ALTIMETER_COEFFS); }
+        unsigned int coeff_1;
+        unsigned int coeff_2;
+        unsigned int coeff_3;
+        unsigned int coeff_4;
+        unsigned int coeff_5;
+        unsigned int coeff_6;
+    };
+    
+    struct ALTIMETER_DATA : GENERIC_MESSAGE {
+        ALTIMETER_DATA() { id = msg::id::ALTIMETER_DATA; size = sizeof(ALTIMETER_DATA); }
+        float pressure_bar;
+        float temp_celcius;
+    };
+    
+    struct HUMIDITY_DATA : GENERIC_MESSAGE {
+        HUMIDITY_DATA() { id = msg::id::HUMIDITY_DATA; size = sizeof(HUMIDITY_DATA); }
+        float relative_humidity;
+        float temp_celcius;
+    };
+    
 }
 
 namespace msg::raw {
@@ -57,6 +82,28 @@ namespace msg::raw {
         int VAR2 : 16;
     };
     
+    struct ALTIMETER_COEFFS : GENERIC_MESSAGE {
+        ALTIMETER_COEFFS() { id = msg::id::ALTIMETER_COEFFS; size = sizeof(ALTIMETER_COEFFS); }
+        int coeff_1 : 16;
+        int coeff_2 : 16;
+        int coeff_3 : 16;
+        int coeff_4 : 16;
+        int coeff_5 : 16;
+        int coeff_6 : 16;
+    };
+    
+    struct ALTIMETER_DATA : GENERIC_MESSAGE {
+        ALTIMETER_DATA() { id = msg::id::ALTIMETER_DATA; size = sizeof(ALTIMETER_DATA); }
+        int pressure_bar : 24;
+        int temp_celcius : 24;
+    };
+    
+    struct HUMIDITY_DATA : GENERIC_MESSAGE {
+        HUMIDITY_DATA() { id = msg::id::HUMIDITY_DATA; size = sizeof(HUMIDITY_DATA); }
+        int relative_humidity : 10;
+        int temp_celcius : 12;
+    };
+    
 }
 
 namespace msg {
@@ -64,18 +111,27 @@ namespace msg {
     struct MessageCollection {
         real::TEST_MESSAGE TEST_MESSAGE;
         real::TEST_MESSAGE_2 TEST_MESSAGE_2;
+        real::ALTIMETER_COEFFS ALTIMETER_COEFFS;
+        real::ALTIMETER_DATA ALTIMETER_DATA;
+        real::HUMIDITY_DATA HUMIDITY_DATA;
     };
     
     union RealMessageUnion {
         RealMessageUnion() { memset( this, 0, sizeof( RealMessageUnion ) ); }
         real::TEST_MESSAGE TEST_MESSAGE;
         real::TEST_MESSAGE_2 TEST_MESSAGE_2;
+        real::ALTIMETER_COEFFS ALTIMETER_COEFFS;
+        real::ALTIMETER_DATA ALTIMETER_DATA;
+        real::HUMIDITY_DATA HUMIDITY_DATA;
     };
     
     union RawMessageUnion {
         RawMessageUnion() { memset( this, 0, sizeof( RawMessageUnion ) ); }
         raw::TEST_MESSAGE TEST_MESSAGE;
         raw::TEST_MESSAGE_2 TEST_MESSAGE_2;
+        raw::ALTIMETER_COEFFS ALTIMETER_COEFFS;
+        raw::ALTIMETER_DATA ALTIMETER_DATA;
+        raw::HUMIDITY_DATA HUMIDITY_DATA;
     };
     
     inline void* getMessageAddressFromCollection(MessageCollection& collection, const id::MessageType type) {
@@ -84,6 +140,12 @@ namespace msg {
                 return &collection.TEST_MESSAGE;
             case msg::id::TEST_MESSAGE_2:
                 return &collection.TEST_MESSAGE_2;
+            case msg::id::ALTIMETER_COEFFS:
+                return &collection.ALTIMETER_COEFFS;
+            case msg::id::ALTIMETER_DATA:
+                return &collection.ALTIMETER_DATA;
+            case msg::id::HUMIDITY_DATA:
+                return &collection.HUMIDITY_DATA;
             default:
                 return nullptr;
         }
@@ -107,6 +169,31 @@ namespace msg::conv {
         return real;
     }
     
+    inline msg::real::ALTIMETER_COEFFS ALTIMETER_COEFFS_TO_REAL(msg::raw::ALTIMETER_COEFFS* raw) {
+        msg::real::ALTIMETER_COEFFS real;
+        real.coeff_1 = (raw->coeff_1 * 1) + 0;
+        real.coeff_2 = (raw->coeff_2 * 1) + 0;
+        real.coeff_3 = (raw->coeff_3 * 1) + 0;
+        real.coeff_4 = (raw->coeff_4 * 1) + 0;
+        real.coeff_5 = (raw->coeff_5 * 1) + 0;
+        real.coeff_6 = (raw->coeff_6 * 1) + 0;
+        return real;
+    }
+    
+    inline msg::real::ALTIMETER_DATA ALTIMETER_DATA_TO_REAL(msg::raw::ALTIMETER_DATA* raw) {
+        msg::real::ALTIMETER_DATA real;
+        real.pressure_bar = (raw->pressure_bar * 2.980232238769531e-07) + 0;
+        real.temp_celcius = (raw->temp_celcius * 7.450580596923828e-06) + -40;
+        return real;
+    }
+    
+    inline msg::real::HUMIDITY_DATA HUMIDITY_DATA_TO_REAL(msg::raw::HUMIDITY_DATA* raw) {
+        msg::real::HUMIDITY_DATA real;
+        real.relative_humidity = (raw->relative_humidity * 0.09765625) + 0;
+        real.temp_celcius = (raw->temp_celcius * 0.040283203125) + -40;
+        return real;
+    }
+    
     inline msg::raw::TEST_MESSAGE TEST_MESSAGE_TO_RAW(msg::real::TEST_MESSAGE* real) {
         msg::raw::TEST_MESSAGE raw;
         raw.test = (real->test - 0) / 1;
@@ -121,6 +208,31 @@ namespace msg::conv {
         return raw;
     }
     
+    inline msg::raw::ALTIMETER_COEFFS ALTIMETER_COEFFS_TO_RAW(msg::real::ALTIMETER_COEFFS* real) {
+        msg::raw::ALTIMETER_COEFFS raw;
+        raw.coeff_1 = (real->coeff_1 - 0) / 1;
+        raw.coeff_2 = (real->coeff_2 - 0) / 1;
+        raw.coeff_3 = (real->coeff_3 - 0) / 1;
+        raw.coeff_4 = (real->coeff_4 - 0) / 1;
+        raw.coeff_5 = (real->coeff_5 - 0) / 1;
+        raw.coeff_6 = (real->coeff_6 - 0) / 1;
+        return raw;
+    }
+    
+    inline msg::raw::ALTIMETER_DATA ALTIMETER_DATA_TO_RAW(msg::real::ALTIMETER_DATA* real) {
+        msg::raw::ALTIMETER_DATA raw;
+        raw.pressure_bar = (real->pressure_bar - 0) / 2.980232238769531e-07;
+        raw.temp_celcius = (real->temp_celcius - -40) / 7.450580596923828e-06;
+        return raw;
+    }
+    
+    inline msg::raw::HUMIDITY_DATA HUMIDITY_DATA_TO_RAW(msg::real::HUMIDITY_DATA* real) {
+        msg::raw::HUMIDITY_DATA raw;
+        raw.relative_humidity = (real->relative_humidity - 0) / 0.09765625;
+        raw.temp_celcius = (real->temp_celcius - -40) / 0.040283203125;
+        return raw;
+    }
+    
     inline msg::id::MessageType convertRawToReal(msg::RealMessageUnion* dest, GENERIC_MESSAGE* raw) {
         switch(raw->id) {
             case msg::id::TEST_MESSAGE:
@@ -128,6 +240,15 @@ namespace msg::conv {
                 break;
             case msg::id::TEST_MESSAGE_2:
                 dest->TEST_MESSAGE_2 = msg::conv::TEST_MESSAGE_2_TO_REAL((msg::raw::TEST_MESSAGE_2*)(raw));
+                break;
+            case msg::id::ALTIMETER_COEFFS:
+                dest->ALTIMETER_COEFFS = msg::conv::ALTIMETER_COEFFS_TO_REAL((msg::raw::ALTIMETER_COEFFS*)(raw));
+                break;
+            case msg::id::ALTIMETER_DATA:
+                dest->ALTIMETER_DATA = msg::conv::ALTIMETER_DATA_TO_REAL((msg::raw::ALTIMETER_DATA*)(raw));
+                break;
+            case msg::id::HUMIDITY_DATA:
+                dest->HUMIDITY_DATA = msg::conv::HUMIDITY_DATA_TO_REAL((msg::raw::HUMIDITY_DATA*)(raw));
                 break;
         }
         return raw->id;
@@ -140,6 +261,15 @@ namespace msg::conv {
                 break;
             case msg::id::TEST_MESSAGE_2:
                 dest->TEST_MESSAGE_2 = msg::conv::TEST_MESSAGE_2_TO_RAW((msg::real::TEST_MESSAGE_2*)(real));
+                break;
+            case msg::id::ALTIMETER_COEFFS:
+                dest->ALTIMETER_COEFFS = msg::conv::ALTIMETER_COEFFS_TO_RAW((msg::real::ALTIMETER_COEFFS*)(real));
+                break;
+            case msg::id::ALTIMETER_DATA:
+                dest->ALTIMETER_DATA = msg::conv::ALTIMETER_DATA_TO_RAW((msg::real::ALTIMETER_DATA*)(real));
+                break;
+            case msg::id::HUMIDITY_DATA:
+                dest->HUMIDITY_DATA = msg::conv::HUMIDITY_DATA_TO_RAW((msg::real::HUMIDITY_DATA*)(real));
                 break;
         }
         return real->id;
@@ -156,6 +286,25 @@ namespace msg::conv {
             sprintf(dest, "%d %.3f \n", 
                     ((msg::real::TEST_MESSAGE_2*)message)->VAR1, 
                     ((msg::real::TEST_MESSAGE_2*)message)->VAR2);
+            break;
+        case msg::id::ALTIMETER_COEFFS:
+            sprintf(dest, "%u %u %u %u %u %u \n", 
+                    ((msg::real::ALTIMETER_COEFFS*)message)->coeff_1, 
+                    ((msg::real::ALTIMETER_COEFFS*)message)->coeff_2, 
+                    ((msg::real::ALTIMETER_COEFFS*)message)->coeff_3, 
+                    ((msg::real::ALTIMETER_COEFFS*)message)->coeff_4, 
+                    ((msg::real::ALTIMETER_COEFFS*)message)->coeff_5, 
+                    ((msg::real::ALTIMETER_COEFFS*)message)->coeff_6);
+            break;
+        case msg::id::ALTIMETER_DATA:
+            sprintf(dest, "%.3f %.3f \n", 
+                    ((msg::real::ALTIMETER_DATA*)message)->pressure_bar, 
+                    ((msg::real::ALTIMETER_DATA*)message)->temp_celcius);
+            break;
+        case msg::id::HUMIDITY_DATA:
+            sprintf(dest, "%.3f %.3f \n", 
+                    ((msg::real::HUMIDITY_DATA*)message)->relative_humidity, 
+                    ((msg::real::HUMIDITY_DATA*)message)->temp_celcius);
             break;
         }
     }
