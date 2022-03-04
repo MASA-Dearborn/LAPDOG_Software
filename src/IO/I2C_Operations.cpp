b23a7405c895a3540a05287e9f9a018554efba32
Lucasring <zanerlo50@gmail.com>
2022-03-04 01:04:44 +0000

    Added single read/write for I2C

--- src/IO/I2C_Operations.cpp
+++ src/IO/I2C_Operations.cpp
@@ -25,6 +25,19 @@ void i2c_operations::_i2c_read(int fileDescriptor, char* dest, const uint32_t nu
 }
 
 /**
+ * @brief   Read one byte from I2C device
+ * 
+ * @param fileDescriptor    an open i2c device file
+ * @return char             the byte read from the device
+ */
+char i2c_operations::_i2c_read_one(int fileDescriptor)
+{
+    char temp;
+    read(fileDescriptor, &temp, 1);
+    return temp
+}
+
+/**
  * @brief   writes to the I2C device
  * 
  * @param fileDescriptor    an open i2c device file
@@ -36,13 +49,26 @@ void i2c_operations::_i2c_write(int fileDescriptor, uint8_t reg, char* src, cons
 {
     static char data[4096]; 
 
-    if (nullptr == src)
+    if (nullptr == src && 0 != num)
         return;
 
     data[0] = reg;
-    memcpy(&data[1], src, num);
+
+    if (num > 0)
+        memcpy(&data[1], src, num);
 
     write(fileDescriptor, data, num + 1);    
+}
+
+/**
+ * @brief   writes one byte to the I2C device
+ * 
+ * @param fileDescriptor    an open i2c device file
+ * @param data              the data to write
+ */
+void i2c_operations::_i2c_write_one(int fileDescriptor, uint8_t data)
+{
+    write(fileDescriptor, data, 1); 
 }
 
 /**
