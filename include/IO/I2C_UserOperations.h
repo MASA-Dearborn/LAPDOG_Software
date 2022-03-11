792e3a8fc1c829df31f6cc8e5fe92e652cf83833
Lucasring <zanerlo50@gmail.com>
2022-03-11 00:49:10 +0000

    i2c updates

--- include/IO/I2C_UserOperations.h
+++ include/IO/I2C_UserOperations.h
@@ -13,13 +13,9 @@ namespace i2c_operations
     };
 
     // Read functions
-    void READ_TEST_MESSAGE(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg);
-    void ALTIMETER_READ_TEMP(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg);
     void ALTIMETER_READ_CONFIG(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg);
-    void ATLIMETER_READ_ALTITUDE(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg);
+    void ALTIMETER_READ_ALTITUDE(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg);
 
     // Delayed callback functions
-    void ALTIMETER_READ_TEMP_CALLBACK(sigval data);
-    void ALTIMETER_READ_CONFIG_CALLBACK(sigval data);
     void ALTIMETER_READ_ALTITUDE_CALLBACK(sigval data);
 }
--- src/IO/I2C_UserOperations.cpp
+++ src/IO/I2C_UserOperations.cpp
@@ -1,45 +1,28 @@
 #include "IO/I2C_Operations.h"
 #include "IO/I2C_UserOperations.h"
+#include "IO/I2C_Devices.h"
 
-void i2c_operations::READ_TEST_MESSAGE(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg)
+void _setup_callback(unsigned long delay_ms, void (*func)(sigval), int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg)
 {
-    _i2c_set_slave_address(fileDescriptor, slave_address);
-
-    char data[64] = {64};
-    _i2c_write(fileDescriptor, 8, data, 4);
-
-    char ret[13] = {0};
-    ret[13] = '\0';
-    _i2c_read(fileDescriptor, ret, 12);
-
-    callback_data temp = {.fileDescriptor = fileDescriptor, .slave_address = slave_address, .msg = msg};
-    Timer callback_timer = Timer(&ALTIMETER_READ_TEMP_CALLBACK, &temp, 0, 40);
+    i2c_operations::callback_data temp = {.fileDescriptor = fileDescriptor, .slave_address = slave_address, .msg = msg};
+    Timer callback_timer = Timer(func, &temp, 0, delay_ms);
 }
 
-void i2c_operations::ALTIMETER_READ_TEMP(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg)
+void i2c_operations::ALTIMETER_READ_ALTITUDE(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg)
 {
-
+    _setup_callback(20, &ALTIMETER_READ_ALTITUDE_CALLBACK, fileDescriptor, slave_address, msg);
 }
 
 void i2c_operations::ALTIMETER_READ_CONFIG(int fileDescriptor, int slave_address, msg::GENERIC_MESSAGE* msg)
 {
+    msg::real::ALTIMETER_COEFFS* m = (msg::real::ALTIMETER_COEFFS*)msg; 
+    _i2c_set_slave_address(fileDescriptor, slave_address);
 
-}
-
-void i2c_operations::ALTIMETER_READ_CONFIG_CALLBACK(sigval data)
-{
+    _i2c_write(fileDescriptor, MS5083_ALTIMETER_COMMANDS::PROM_READ_COEFF_1, NULL, 0);
 
 }
 
 void i2c_operations::ALTIMETER_READ_ALTITUDE_CALLBACK(sigval data)
 {
 
-}
-
-void i2c_operations::ALTIMETER_READ_TEMP_CALLBACK(sigval data)
-{
-    char ret[16] = {0};
-    callback_data* c = (callback_data*)data.sival_ptr;
-    _i2c_set_slave_address(c->fileDescriptor, c->slave_address);
-    _i2c_read(c->fileDescriptor, ret, 4);
 }
